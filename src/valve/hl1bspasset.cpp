8ee5a773f7aa1e5f7065007666e29e253a798714
Wouter Saaltink <wouter.saaltink@gmail.com>
2017-01-04 01:05:03 +0100

    fixed missing stb image write

--- src/valve/hl1bspasset.cpp
+++ src/valve/hl1bspasset.cpp
@@ -2,8 +2,6 @@
 #include "valve/hl1bspinstance.h"
 #define STB_RECT_PACK_IMPLEMENTATION
 #include "stb_rect_pack.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
 
 #include <iostream>
 #include <sstream>
